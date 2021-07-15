4f63f3132e93c92b12b964b7689bb606eba0ab8f
Dillon Min <dillon.minfei@gmail.com>
2021-07-15 16:00:54 +0800

    media: v4l: uapi: Add user control base for stm32 dma2d controls
    Add a control base for stm32 controls, and reserve 16 controls.
    
    Signed-off-by: Dillon Min <dillon.minfei@gmail.com>

---
+++ include/uapi/linux/stm32-media.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-only WITH Linux-syscall-note */
+/* Copyright (C) 2021 Dillon Min, <dillon.minfei@gmail.com> */
+
+#ifndef __UAPI_STM32_MEDIA_H__
+#define __UAPI_STM32_MEDIA_H__
+
+#include <linux/v4l2-controls.h>
+
+#define V4L2_CID_DMA2D_R2M_COLOR		(V4L2_CID_USER_STM32_BASE + 1)
+#define V4L2_CID_DMA2D_R2M_MODE			(V4L2_CID_USER_STM32_BASE + 2)
+
+#endif
